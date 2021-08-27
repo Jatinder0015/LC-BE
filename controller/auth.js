aad0416595acbe6ead1c9799dd8dc309c86abd36
Jatinder0015 <51458242+Jatinder0015@users.noreply.github.com>
2021-08-27 17:31:20 +0530

    updated delete by id

--- controller/auth.js
+++ controller/auth.js
@@ -381,3 +381,21 @@ exports.userUpdate = async (req, res, next) => {
       next(err);
     });
 };
+
+exports.deleteUser = (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(userId)
+  User.findByPk(userId)
+    .then(user => {
+      console.log(user);
+      user.destroy().then(result=>{
+      res.status(200).json({ message: 'User deleted' });
+    })})
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    }
+    );
+};
